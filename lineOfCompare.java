ffc8b30ebffdd9f2b22ae051bfcd235fbde7cf39
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-15 01:14:36 +0530

    UC-4: Calculate difference and check Equality & Comparison of

--- lineOfCompare.java
+++ lineOfCompare.java
@@ -5,6 +5,7 @@ public class lineOfCompare
 	public int x1,x2,y1,y2;
 	public double lengthOfLine;
 	public double line1,line2;
+
 	public double lengthCalculation()
 	{
 		Scanner sc = new Scanner(System.in);
@@ -18,32 +19,34 @@ public class lineOfCompare
 
 		return lengthOfLine;
 	}
+
 	public void comparisonOfLine()
 	{
 		lengthCalculation();
 
-		System.out.println(" ************* Distance Between Line X and Y ************** ");
+		System.out.println(" ************* Distance Between Line X and Y ***************");
 		System.out.println("Value of X1:-" +x1+ "\t\t Value of Y1:-"+y1);
 		System.out.println("Value of X2:-" +x2+ "\t\t Value of Y2:-"+y2);
-		System.out.println("***********************************************************");
+		System.out.println("------------------------------------------------------------");
                	System.out.println("Distance Between Line is:-"+lengthOfLine);
+		System.out.println("************************************************************");
+
 	}
 
 	public  void compareLines()
 	{
 		System.out.println("-------------------------------------------------Line 1---------------------------------------------------");
 		comparisonOfLine();
-		line1=lengthOfLine;
 		Integer length1 = (int) lengthOfLine;
+		line1=lengthOfLine;
 
 		System.out.println("-------------------------------------------------Line 2---------------------------------------------------");
                 comparisonOfLine();
-                line2=lengthOfLine;
-
 		Integer length2 = (int) lengthOfLine;
+		line2=lengthOfLine;
 
+		System.out.println("-----------------------Comparison Of Lines----------------------------");
 		int compare = length1.compareTo(length2);
-
 		if(length1 > length2)
 		{
 			 System.out.println("Line 1 is Greater");
@@ -55,6 +58,16 @@ public class lineOfCompare
 		else
 		{
 			 System.out.println("Both are Equals");
+		}
+
+		System.out.println("-----------------------Equality Of Lines----------------------------");
+		if(line1 == line2)
+		{
+			 System.out.println("Lines Are Equal");
+		}
+		else
+		{
+			 System.out.println("Lines Are Not Equal");
 		}
 	}
 	public static void main(String args[])
